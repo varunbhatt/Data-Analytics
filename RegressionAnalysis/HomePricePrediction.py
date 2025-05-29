a608717b35a7a112894be358994a5b24b5748e2d
BHATT Varun <V6T@dsone.3ds.com>
2025-05-29 11:28:53 +0530

    Regression Anlysis

--- RegressionAnalysis/HomePricePrediction.py
+++ RegressionAnalysis/HomePricePrediction.py
@@ -5,8 +5,140 @@ import matplotlib.pyplot as plt
 
 import numpy as np
 
+#import os
+#cwd = os.getcwd()  # Get the current working directory (cwd)
+#files = os.listdir(cwd)  # Get all the files in that directory
+#print("Files in %r: %s" % (cwd, files))
+
 housing_df = pd.read_csv('housing.csv')
 
 plt.hist(housing_df['median_house_value'], bins=80)
-
 plt.show()
+
+#Prepare Data
+
+#(1) Handle Missing data using Impute
+#print(housing_df.isnull().sum())
+
+from sklearn.impute import KNNImputer
+
+# create a temporary copy of the dataset
+housing_df_temp = housing_df.copy()
+
+# retrieve columns with numerical data; will exclude the ocean_proximity column since the datatype is object; other columns are float64
+columns_list = [col for col in housing_df_temp.columns if housing_df_temp[col].dtype != 'object']
+
+# extract columns that contain at least one missing value
+new_column_list = [col for col in housing_df_temp.loc[:, housing_df_temp.isnull().any()]]
+
+# update temp dataframe with numeric columns that have empty values
+housing_df_temp = housing_df_temp[new_column_list]
+
+# initialize KNNImputer to impute missing data using machine learning
+knn = KNNImputer(n_neighbors = 3)
+
+# fit function trains the model 
+knn.fit(housing_df_temp)
+
+# transform the data using the model
+# applies the transformation model (ie knn) to data
+array_Values = knn.transform(housing_df_temp)
+
+# convert the array values to a dataframe with the appropriate column names
+housing_df_temp = pd.DataFrame(array_Values, columns = new_column_list)
+
+# overlay the imputed column over the old column with missing values
+
+# loop through the list of columns and overlay each one
+for column_name in new_column_list:
+    housing_df[column_name] = housing_df_temp.replace(housing_df[column_name],housing_df[column_name])
+
+# confirm columns no longer contain null data
+#print(housing_df.isnull().sum())
+
+#2 Removing Correlated Features
+# Plot a graphical correlation matrix for each pair of columns in the dataframe
+corr = housing_df.corr(numeric_only=True) # data frame correlation function
+# make the heatmap larger in size
+plt.figure(figsize = (8,8))
+
+sns.heatmap(corr, annot=True)
+plt.show()
+
+# Additionally we noted that several features (total_rooms,total_bedrooms,population,households) have very high correlation to one another, 
+# so it's interesting to find out if a removal of a few of them would have any affect on the model performance
+
+#  a new feature that is a ratio of the total rooms to households
+housing_df['rooms_per_household'] = housing_df['total_rooms']/housing_df['households']
+
+# a new feature that is a ratio of the total bedrooms to the total rooms 
+housing_df['bedrooms_per_room'] = housing_df['total_bedrooms']/housing_df['total_rooms']
+
+# a new feature that is a ratio of the population to the households 
+housing_df['population_per_household']= housing_df['population']/housing_df['households']
+
+# let's combine the latitude and longitude into 1
+housing_df['coords'] = housing_df['longitude']/housing_df['latitude']
+
+# remove total_rooms, households, total bedrooms, popluation, longitude, latitude
+housing_df = housing_df.drop('total_rooms', axis=1)
+housing_df = housing_df.drop('households', axis=1)
+housing_df = housing_df.drop('total_bedrooms', axis=1)
+housing_df = housing_df.drop('population', axis=1)
+housing_df = housing_df.drop('longitude', axis=1)
+housing_df = housing_df.drop('latitude', axis=1)
+
+corr = housing_df.corr(numeric_only=True) 
+
+#make the heatmap larger in size
+plt.figure(figsize = (7,7))
+
+sns.heatmap(corr, annot=True)
+plt.show()
+
+#(3)Encode Catagory data
+# let's replace the OCEAN_PROXIMITY column using get_dummies()
+housing_df_encoded = pd.get_dummies(data=housing_df, columns=['ocean_proximity'])
+
+
+#Train the Model
+import sklearn
+from sklearn.model_selection import train_test_split
+
+# remove spaces from column names and convert all to lowercase and remove special characters as it could cause issues in the future
+housing_df_encoded.columns = [c.lower().replace(' ', '_').replace('<', '_') for c in housing_df_encoded.columns]
+
+# Split target variable and feature variables
+X = housing_df_encoded[['housing_median_age', 'median_income','bedrooms_per_room','population_per_household','coords','ocean_proximity__1h_ocean',
+                        'ocean_proximity_inland','ocean_proximity_island','ocean_proximity_near_bay','ocean_proximity_near_ocean']]
+y = housing_df_encoded['median_house_value']
+
+# Splitting the data into training and testing sets in numpy arrays
+# We train the model with 70% of the samples and test with the remaining 30%
+# X -> array with the inputs; y -> array of the outputs
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, shuffle=True, test_size=0.3)
+
+# Use scikit-learn’s LinearRegression to train the model on both the training and evaluate it on the test sets
+from sklearn.linear_model import LinearRegression
+
+# Create a Linear regressor using all the feature variables
+reg_model = LinearRegression()
+
+# Train the model using the training sets
+reg_model.fit(X_train, y_train)
+
+
+
+#Prediction on Train Model
+#run the predictions on the training and testing data
+y_pred_test = reg_model.predict(X_test)
+
+
+# Determine accuracy uisng 𝑅^2
+# 𝑅^2 : R squared is another way to evaluate the performance of a regression model.
+# 1, means that the model is perfect and 0 means the the model will perform poorly.
+from sklearn.metrics import r2_score, mean_squared_error
+
+score = r2_score(y_test, y_pred_test)
+
+print("R^2 - {}%".format(round(score, 2) *100))
